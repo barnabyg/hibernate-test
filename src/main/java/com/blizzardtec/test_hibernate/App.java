da3f8d461ddd3048723ba77e87c2b0328ff389b8
barnabyg <barnabyg@blizzardtec.com>
2014-06-02 11:42:13 +0100

    first add

---
+++ src/main/java/com/blizzardtec/test_hibernate/App.java
@@ -0,0 +1,13 @@
+package com.blizzardtec.test_hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
